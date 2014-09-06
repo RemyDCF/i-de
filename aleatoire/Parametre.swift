20cd1c9b9336817ad892eae0ae3934410f5bdafb
DA COSTA FARO Rémy <remy@dacostafaro.com>
2014-09-06 15:04:59 +0200

    Aléatoire version 0.3 bêta 3
    *** Nouveautés ***
    - Ajout de notification
    - Simplification du système de choix des faces du dé
    - Technique : Ajout d'un dossier pour les images GIMP
    
    *********************

--- aleatoire/Parametre.swift
+++ aleatoire/Parametre.swift
@@ -11,10 +11,29 @@ import UIKit
 class Parametre: UITableViewController {
     let pref = NSUserDefaults.standardUserDefaults()
     @IBOutlet weak var segmentChoixDe: UISegmentedControl!
+    @IBOutlet weak var segmentChoixNombreFace: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         if (pref.boolForKey("secouer") == false) {
             segmentChoixDe.selectedSegmentIndex = 1
+        }
+        var donnee = MesDonnes()
+        var dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        var path = dir[0] . stringByAppendingPathComponent("nombreFace")
+        if (NSFileManager.defaultManager().fileExistsAtPath(path)) {
+            donnee = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as MesDonnes
+            if (donnee.laChaine == "6") {
+                segmentChoixNombreFace.selectedSegmentIndex = 0
+            }
+            if (donnee.laChaine == "8") {
+                segmentChoixNombreFace.selectedSegmentIndex = 1
+            }
+            if (donnee.laChaine == "10") {
+                segmentChoixNombreFace.selectedSegmentIndex = 1
+            }
+        }
+        else {
+            segmentChoixNombreFace.selectedSegmentIndex = 0
         }
     }
 
@@ -26,6 +45,7 @@ class Parametre: UITableViewController {
     override func prefersStatusBarHidden() -> Bool  {
         return true
     }
+    
     @IBAction func choixTypeLanceDeChange(sender: AnyObject) {
         var appDefault:NSDictionary?
         if (segmentChoixDe.selectedSegmentIndex == 0) {
@@ -37,5 +57,51 @@ class Parametre: UITableViewController {
         println(appDefault)
         pref.registerDefaults(appDefault!)
         pref.synchronize()
+        afficherNotification(false)
+    }
+    
+    @IBAction func choixNombreFaceChange(sender: AnyObject) {
+        super.viewDidLoad()
+        var donnee = MesDonnes()
+        if (segmentChoixNombreFace.selectedSegmentIndex == 0) {
+            donnee.laChaine == "6"
+        }
+        if (segmentChoixNombreFace.selectedSegmentIndex == 1) {
+            donnee.laChaine == "8"
+        }
+        if (segmentChoixNombreFace.selectedSegmentIndex == 2) {
+            donnee.laChaine == "10"
+        }
+        var dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        var path = dir[0] . stringByAppendingPathComponent("nombreFace")
+        var erreur = NSKeyedArchiver.archiveRootObject(donnee, toFile: path)
+        if !erreur {
+            afficherNotification(true)
+        }
+        else {
+            afficherNotification(false)
+        }
+    }
+    
+    func afficherNotification(erreur: Bool) {
+        var icone:UIImage! = nil
+        if (erreur == true) {
+            icone = UIImage(named: "iconeErreur")
+        }
+        else {
+            icone = UIImage(named: "iconeOK")
+        }
+        var titre:String! = "Changement effectué"
+        var couleur:UIColor! = UIColor.greenColor()
+        if (erreur == true) {
+            titre = "Erreur de changement"
+            couleur = UIColor.redColor()
+        }
+        let notification = MPGNotification(title: titre, subtitle: nil, backgroundColor: couleur, iconImage: icone)
+        notification.setButtonConfiguration(MPGNotificationButtonConfigration.ZeroButtons, withButtonTitles: nil)
+        notification.duration = 2.0
+        notification.titleColor = UIColor.whiteColor()
+        notification.animationType = MPGNotificationAnimationType.Linear
+        notification.show()
     }
 }
